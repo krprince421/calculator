0cdcf695554b6ad8ce7741d6ebdedefcb5b2611f
krprince421 <61113784+krprince421@users.noreply.github.com>
2022-06-04 00:05:25 +0530

    firstCommit

---
+++ index.js
@@ -0,0 +1,44 @@
+var x = 0 ;
+
+function digitClick( x ){
+    document.getElementById('expression').value += x ;
+}
+
+function clearExpression(){
+    document.getElementById('expression').value = '' ;
+}
+function changeSign(){
+    var equation = document.getElementById('expression').value ;
+    if( ! isNaN( equation) ){
+        document.getElementById('expression').value *= -1 ;
+    }
+}
+
+function printResult(){
+    var equation = document.getElementById('expression').value ;
+    try{
+        document.getElementById("expression").value = eval( equation ) ;
+    }catch( err ){
+        document.getElementById("expression").value = "Invalid Equation" ;   
+        
+    }
+        
+}
+
+function myFunction(){
+
+    var input = document.getElementById('expression') ;
+
+    input.addEventListener("keypress" , function(event){
+        if( event.key === 'Enter' )printResult() ;
+        if( event.key === '=' ){
+            printResult() ;
+
+        }
+        
+    }) ;
+
+
+}
+
+
